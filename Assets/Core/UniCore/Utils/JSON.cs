721a24400960563e8f0df61782a4efb66bbf3dac
Nevrozelya <martinchertier@gmail.com>
2023-05-20 19:02:18 +0200

    fix: json utils

--- Assets/Core/UniCore/Utils/JSON.cs
+++ Assets/Core/UniCore/Utils/JSON.cs
@@ -1,62 +1,70 @@
-﻿namespace CTA.Core.Utils
+﻿using Newtonsoft.Json;
+using System;
+using UniCore.Utils;
+
+namespace CTA.Core.Utils
 {
     public static class JSON
     {
-        //public static bool TryDeserialize<T>(string json, out T result)
-        //{
-        //    try
-        //    {
-        //        result = JsonConvert.DeserializeObject<T>(json);
-        //        if (result == null)
-        //        {
-        //            return false;
-        //        }
-        //        return true;
-        //    }
-        //    catch (System.Exception e)
-        //    {
-        //        Log.Error($"{e.Message}", "JSON");
-        //        result = default(T);
-        //        return false;
-        //    }
-        //}
+        private const string LOG = "JSON";
 
-        //public static bool TrySerialize(object obj, out string json, bool indent = true)
-        //{
-        //    Formatting formatting = indent ? Formatting.Indented : Formatting.None;
-        //    try
-        //    {
-        //        json = JsonConvert.SerializeObject(obj, formatting, new JsonSerializerSettings
-        //        {
-        //            NullValueHandling = NullValueHandling.Ignore
-        //        });
+        public static bool TryDeserialize<T>(string json, out T result)
+        {
+            try
+            {
+                result = JsonConvert.DeserializeObject<T>(json);
+                if (result == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Logg.Error(e.Message, LOG);
+                result = default(T);
+                return false;
+            }
+        }
 
-        //        if (string.IsNullOrWhiteSpace(json))
-        //        {
-        //            return false;
-        //        }
-        //        return true;
-        //    }
-        //    catch // (Exception e)
-        //    {
-        //        json = string.Empty;
-        //        return false;
-        //    }
-        //}
+        public static bool TrySerialize(object obj, out string json, bool indent = true)
+        {
+            Formatting formatting = indent ? Formatting.Indented : Formatting.None;
 
-        //public static bool TryRemap<T>(object input, out T remapped)
-        //{
-        //    remapped = default;
+            try
+            {
+                json = JsonConvert.SerializeObject(obj, formatting, new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                });
 
-        //    if (TrySerialize(input, out string json, indent: false))
-        //    {
-        //        if (TryDeserialize(json, out remapped))
-        //        {
-        //            return true;
-        //        }
-        //    }
+                if (string.IsNullOrWhiteSpace(json))
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Logg.Error(e.Message, LOG);
+                json = string.Empty;
+                return false;
+            }
+        }
 
-        //    return false;
-        //}
+        public static bool TryRemap<T>(object input, out T remapped)
+        {
+            remapped = default;
+
+            if (TrySerialize(input, out string json, indent: false))
+            {
+                if (TryDeserialize(json, out remapped))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
