f1e4506083136f19834bb00bab321b7f3aa516d9
Nevrozelya <martinchertier@gmail.com>
2023-06-18 22:30:13 +0200

    feat: renderer extensions

---
+++ Assets/Core/UniCore/Extensions/RendererExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UniCore.Extensions
+{
+    public static class RendererExtensions
+    {
+        public const string EMISSION_COLOR = "_EmissionColor";
+
+        public static void SetEmission(this Renderer renderer, Color color)
+        {
+            renderer.material.SetColor(EMISSION_COLOR, color);
+        }
+    }
+}
