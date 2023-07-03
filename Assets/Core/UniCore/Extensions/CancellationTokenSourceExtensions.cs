de460ffa088c021bc63441d97e38c2968565b110
Nevrozelya <martinchertier@gmail.com>
2023-07-04 00:10:47 +0200

    feat: CancellationTokenSource extensions

---
+++ Assets/Core/UniCore/Extensions/CancellationTokenSourceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+
+namespace UniCore.Extensions
+{
+    public static class CancellationTokenSourceExtensions
+    {
+        public static void CancelAndDispose(this CancellationTokenSource token)
+        {
+            if (token != null)
+            {
+                token.Cancel();
+                token.Dispose();
+            }
+        }
+    }
+}
