00f4b98d2565457c96eb504a5bf6d81533f50d11
Nevrozelya <martinchertier@gmail.com>
2023-07-09 00:42:29 +0200

    feat: annotation component

---
+++ Assets/Core/UniCore/Components/Annotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace UniCore.Components
+{
+    public class Annotation : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [TextArea(10, 50)][SerializeField] private string _annotation;
+#endif
+    }
+}
