4047b7e4c1bfcbf269bd557d5cb081305cbd7ca9
Nevrozelya <martinchertier@gmail.com>
2023-08-05 17:07:39 +0200

    fix: cancellatioon token source extension try/catch

--- Assets/Core/UniCore/Extensions/CancellationTokenSourceExtensions.cs
+++ Assets/Core/UniCore/Extensions/CancellationTokenSourceExtensions.cs
@@ -6,11 +6,21 @@ namespace UniCore.Extensions
     {
         public static void CancelAndDispose(this CancellationTokenSource token)
         {
-            if (token != null)
+            if (token == null)
+            {
+                return;
+            }
+
+            // Try/catch needed in case the token has already been disposed,
+            // and there is no easy way to check if already disposed
+            // without reflection or token class inheritance.
+
+            try
             {
                 token.Cancel();
                 token.Dispose();
             }
+            catch { }
         }
     }
 }
