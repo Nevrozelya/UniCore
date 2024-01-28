c84a46ba89aa5c4912ab7d615a2b1e61a7d782bb
Nevrozelya <martinchertier@gmail.com>
2024-01-28 18:43:10 +0100

    feat: random color & vector3 xy/xz/yz extensions

--- Assets/UniCore/Extensions/VectorExtensions.cs
+++ Assets/UniCore/Extensions/VectorExtensions.cs
@@ -4,6 +4,8 @@ namespace UniCore.Extensions
 {
     public static class VectorExtensions
     {
+        // Vector 2
+
         public static Vector2 WithX(this Vector2 v, float x)
         {
             return new Vector2(x, v.y);
@@ -13,6 +15,8 @@ namespace UniCore.Extensions
         {
             return new Vector2(v.x, y);
         }
+
+        // Vector 3
 
         public static Vector3 WithX(this Vector3 v, float x)
         {
@@ -27,6 +31,21 @@ namespace UniCore.Extensions
         public static Vector3 WithZ(this Vector3 v, float z)
         {
             return new Vector3(v.x, v.y, z);
+        }
+
+        public static Vector3 WithXY(this Vector3 v, float x, float y)
+        {
+            return new Vector3(x, y, v.z);
+        }
+
+        public static Vector3 WithXZ(this Vector3 v, float x, float z)
+        {
+            return new Vector3(x, v.y, z);
+        }
+
+        public static Vector3 WithYZ(this Vector3 v, float y, float z)
+        {
+            return new Vector3(v.x, y, z);
         }
     }
 }
---
+++ Assets/UniCore/Utils/ColorUtils.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UniCore.Utils
+{
+    public static class ColorUtils
+    {
+        public static Color Random()
+        {
+            float r = UnityEngine.Random.Range(0f, 1f);
+            float g = UnityEngine.Random.Range(0f, 1f);
+            float b = UnityEngine.Random.Range(0f, 1f);
+
+            return new Color(r, g, b);
+        }
+    }
+}
