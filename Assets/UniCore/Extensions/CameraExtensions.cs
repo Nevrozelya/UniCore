73805dfd5dcdf109ea4fb08b03f7c058195c3394
Nevrozelya <martinchertier@gmail.com>
2024-02-01 18:52:31 +0100

    feat: camera extension for othographic world screen size

---
+++ Assets/UniCore/Extensions/CameraExtensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UniCore.Extensions
+{
+    public static class CameraExtensions
+    {
+        public static Vector2 OthographicWorldScreenSize(this Camera camera)
+        {
+            float screenRatio = (float)Screen.width / Screen.height;
+            float worldHeight = camera.orthographicSize * 2;
+            float worldWidth = worldHeight * screenRatio;
+
+            return new Vector2(worldWidth, worldHeight);
+        }
+    }
+}
