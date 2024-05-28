23249cddbe940775a9d74b78a4762707e44b80d4
Nevrozelya <martinchertier@gmail.com>
2024-05-29 00:23:22 +0200

    feat: denormalize implementation

--- Assets/UniCore/Utils/MathUtils.cs
+++ Assets/UniCore/Utils/MathUtils.cs
@@ -8,31 +8,54 @@ namespace UniCore.Utils
 
         public static float Normalize(float value, float sourceScaleMin, float sourceScaleMax)
         {
-            if (sourceScaleMax - sourceScaleMin == 0)
-                return 0;
+            float sourceSize = sourceScaleMax - sourceScaleMin;
 
-            return (value - sourceScaleMin) / (sourceScaleMax - sourceScaleMin);
+            if (sourceSize == 0)
+            {
+                return 0;
+            }
+
+            return (value - sourceScaleMin) / sourceSize;
+        }
+
+        public static float Denormalize(float normalizedValue, float targetScaleMin, float targetScaleMax)
+        {
+            float clamped = Mathf.Clamp01(normalizedValue);
+
+            return GetScaleToScaleValue(clamped, 0, 1, targetScaleMin, targetScaleMax);
         }
 
         public static float GetScaleToScaleValue(float value, float sourceScaleMin, float sourceScaleMax, float targetScaleMin, float targetScaleMax)
         {
+            float delta = value - sourceScaleMin;
+
+            if (delta == 0)
+            {
+                return targetScaleMin;
+            }
+
             float sourceSize = sourceScaleMax - sourceScaleMin;
             float targetSize = targetScaleMax - targetScaleMin;
 
-            float valuePercentage = sourceSize / (value - sourceScaleMin);
+            if (sourceSize == 0)
+            {
+                return targetScaleMin;
+            }
+
+            float valuePercentage = sourceSize / delta;
             float targetValue = targetScaleMin + (targetSize / valuePercentage);
+
             return targetValue;
         }
 
         public static float GetRadianAngle(Vector2 me, Vector2 target)
         {
-            float angle = Mathf.Atan2(target.y - me.y, target.x - me.x);
-            return angle;
+            return Mathf.Atan2(target.y - me.y, target.x - me.x);
         }
 
         public static float GetRadianAngle(Vector2 direction)
         {
-            return GetRadianAngle(new Vector2(0, 0), direction);
+            return GetRadianAngle(Vector2.zero, direction);
         }
 
         public static Vector2 GetDirection(float radianAngle)
@@ -41,7 +64,7 @@ namespace UniCore.Utils
             float y = Mathf.Sin(radianAngle);
 
             // Result is normalized by default
-            // (ofc, because it uses the trigo circle)
+            // (ofc, because it uses the trigonometric circle)
             return new Vector2(x, y);
         }
     }
