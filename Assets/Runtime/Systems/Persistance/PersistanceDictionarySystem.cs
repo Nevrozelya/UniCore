ea51d1b771c7f9b58e66bd86a3ef5a625d2b2fc0
Nevrozelya <martinchertier@gmail.com>
2024-10-03 13:01:17 +0200

    refactor: persistance file & dictionary systems

---
+++ Assets/Runtime/Systems/Persistance/PersistanceDictionarySystem.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using UniCore.Assets.Runtime.Systems.Persistance;
+using UniCore.Extensions;
+
+namespace UniCore.Systems.Persistance
+{
+    public class PersistanceDictionarySystem<T> : PersistanceFileSystem<Dictionary<T, string>>
+    {
+        public PersistanceDictionarySystem(string relativeFilePath) : base(relativeFilePath) { }
+
+        // The write parameter permits to make multiple sequential sets
+        // without triggering multiple file writing, but only for last call.
+        public void Set(T setting, string value, bool write = true)
+        {
+            _data ??= new();
+            _data[setting] = value;
+
+            if (write)
+            {
+                Write();
+            }
+        }
+
+        public void Set(T setting, object value, bool write = true)
+        {
+            string text = value == null ? string.Empty : value.ToString();
+            Set(setting, text, write);
+        }
+
+        public string GetString(T setting)
+        {
+            if (_data.IsNullOrEmpty())
+            {
+                return null;
+            }
+
+            if (!_data.ContainsKey(setting))
+            {
+                return null;
+            }
+
+            return _data[setting];
+        }
+
+        public bool? GetBool(T setting)
+        {
+            string value = GetString(setting);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return null;
+            }
+
+            if (bool.TryParse(value, out bool parsed))
+            {
+                return parsed;
+            }
+
+            return null;
+        }
+
+        public int? GetInt(T setting)
+        {
+            string value = GetString(setting);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return null;
+            }
+
+            if (int.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture.NumberFormat, out int parsed))
+            {
+                return parsed;
+            }
+
+            return null;
+        }
+
+        public float? GetFloat(T setting)
+        {
+            string value = GetString(setting);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return null;
+            }
+
+            value = value.Replace(',', '.');
+
+            if (float.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture.NumberFormat, out float parsed))
+            {
+                return parsed;
+            }
+
+            return null;
+        }
+
+        public TEnum? GetEnum<TEnum>(T setting) where TEnum : struct
+        {
+            string value = GetString(setting);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return null;
+            }
+
+            if (Enum.TryParse(value, out TEnum parsed))
+            {
+                return parsed;
+            }
+
+            return null;
+        }
+
+        public DateTime? GetDate(T setting)
+        {
+            string value = GetString(setting);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return null;
+            }
+
+            if (DateTime.TryParse(value, out DateTime parsed))
+            {
+                return parsed;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Assets/Runtime/Systems/Persistance/PersistanceFileSystem.cs
@@ -0,0 +1,84 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.IO;
+using System.Threading;
+using UniCore.Extensions;
+using UniCore.Utils;
+using UnityEngine;
+
+namespace UniCore.Assets.Runtime.Systems.Persistance
+{
+    public class PersistanceFileSystem<T> : IDisposable
+    {
+        protected T _data;
+
+        private string _path;
+        private CancellationTokenSource _readToken;
+        private CancellationTokenSource _writeToken;
+
+        public bool IsInitialized { get; private set; }
+        public bool IsValid => _data != null;
+
+        public PersistanceFileSystem(string relativeFilePath)
+        {
+            // relativeFilePath must be containing filename & extension
+            _path = Path.Combine(Application.persistentDataPath, relativeFilePath).SanitizeBackslashes();
+            Read();
+        }
+
+        public void Dispose()
+        {
+            _readToken.CancelAndDispose();
+            _writeToken.CancelAndDispose();
+        }
+
+        public T Get()
+        {
+            return _data;
+        }
+
+        public void Set(T data, bool write = true)
+        {
+            _data = data;
+
+            if (write)
+            {
+                Write();
+            }
+        }
+
+        private void Read()
+        {
+            _readToken = _readToken.Renew();
+            ReadAsync(_readToken.Token).Forget();
+        }
+
+        private async UniTask ReadAsync(CancellationToken token)
+        {
+            if (string.IsNullOrWhiteSpace(_path))
+            {
+                return;
+            }
+
+            _data = await FileUtils.ReadAndParseAsync<T>(_path, token);
+
+            IsInitialized = true;
+        }
+
+        protected void Write()
+        {
+            _writeToken = _writeToken.Renew();
+            WriteAsync(_writeToken.Token).Forget();
+        }
+
+        private async UniTask WriteAsync(CancellationToken token)
+        {
+            if (string.IsNullOrWhiteSpace(_path))
+            {
+                return;
+            }
+
+            await FileUtils.SerializeAndWriteAsync(_path, _data, token);
+        }
+    }
+}
