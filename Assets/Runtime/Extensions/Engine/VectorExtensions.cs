bc4a70ec9d31695e5af262bd2d47504f58785e8f
Nevrozelya <martinchertier@gmail.com>
2025-03-09 19:11:14 +0100

    feat: vector extensions add methods

--- Assets/Runtime/Extensions/Engine/VectorExtensions.cs
+++ Assets/Runtime/Extensions/Engine/VectorExtensions.cs
@@ -16,6 +16,16 @@ namespace UniCore.Extensions.Engine
             return new Vector2(v.x, y);
         }
 
+        public static Vector2 AddX(this Vector2 v, float x)
+        {
+            return new Vector2(v.x + x, v.y);
+        }
+
+        public static Vector2 AddY(this Vector2 v, float y)
+        {
+            return new Vector2(v.x, v.y + y);
+        }
+
         // Vector 3
 
         public static Vector3 WithX(this Vector3 v, float x)
@@ -46,6 +56,21 @@ namespace UniCore.Extensions.Engine
         public static Vector3 WithYZ(this Vector3 v, float y, float z)
         {
             return new Vector3(v.x, y, z);
+        }
+
+        public static Vector3 AddX(this Vector3 v, float x)
+        {
+            return new Vector3(v.x + x, v.y, v.z);
+        }
+
+        public static Vector3 AddY(this Vector3 v, float y)
+        {
+            return new Vector3(v.x, v.y + y, v.z);
+        }
+
+        public static Vector3 AddZ(this Vector3 v, float z)
+        {
+            return new Vector3(v.x, v.y, v.z + z);
         }
     }
 }
