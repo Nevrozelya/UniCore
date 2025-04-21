95fc464265f23c8d05934012577148a93b772f52
Nevrozelya <martinchertier@gmail.com>
2025-04-21 13:32:46 +0200

    feat: orthographic size from world width in CameraExtensions

--- Assets/Runtime/Extensions/Engine/CameraExtensions.cs
+++ Assets/Runtime/Extensions/Engine/CameraExtensions.cs
@@ -4,13 +4,27 @@ namespace UniCore.Extensions.Engine
 {
     public static class CameraExtensions
     {
-        public static Vector2 OthographicWorldScreenSize(this Camera camera)
+        public static Vector2 GetOrthographicWorldScreenSize(this Camera camera)
         {
             float screenRatio = (float)Screen.width / Screen.height;
             float worldHeight = camera.orthographicSize * 2;
             float worldWidth = worldHeight * screenRatio;
 
             return new Vector2(worldWidth, worldHeight);
+        }
+
+        public static void SetOrthographicSizeFromWorldWidth(this Camera camera, float width)
+        {
+            camera.orthographicSize = GetOrthographicSizeFromWorldWidth(width);
+        }
+
+        public static float GetOrthographicSizeFromWorldWidth(float width)
+        {
+            float screenRatio = (float)Screen.width / Screen.height;
+            float height = width / screenRatio;
+            float orthographicSize = height / 2f;
+
+            return orthographicSize;
         }
     }
 }
