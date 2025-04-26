0b415301e7f36265713a5035e749c8c74d0f2cdf
Nevrozelya <martinchertier@gmail.com>
2025-04-26 14:26:38 +0200

    feat: shuffle possible on IEnumerable and ICollection

--- Assets/Runtime/Extensions/Language/CollectionRandomExtensions.cs
+++ Assets/Runtime/Extensions/Language/CollectionRandomExtensions.cs
@@ -73,16 +73,39 @@ namespace UniCore.Extensions.Language
         }
 
         // Nasty duplication but better than abusing ToArray() & ToList()
-        public static List<T> Shuffle<T>(this List<T> items, Random rng = null)
+        public static List<T> Shuffle<T>(this ICollection<T> collection, Random rng = null)
         {
-            if (items.IsNullOrEmpty())
+            if (collection.IsNullOrEmpty())
             {
-                return items;
+                return null;
             }
 
             rng ??= new();
 
-            List<T> result = new(items);
+            List<T> result = new(collection);
+
+            for (int i = 0; i < result.Count - 1; i++)
+            {
+                int j = rng.Next(i, result.Count);
+
+                T temp = result[i];
+                result[i] = result[j];
+                result[j] = temp;
+            }
+
+            return result;
+        }
+
+        public static List<T> Shuffle<T>(this IEnumerable<T> enumerable, Random rng = null)
+        {
+            if (enumerable.IsNullOrEmpty())
+            {
+                return null;
+            }
+
+            rng ??= new();
+
+            List<T> result = new(enumerable);
 
             for (int i = 0; i < result.Count - 1; i++)
             {
