0f17afc7ea5d09406def018f0fe4ad37ca4848c5
Nevrozelya <martinchertier@gmail.com>
2025-06-03 15:59:03 +0200

    feat: cancellation group

---
+++ Assets/Runtime/Helpers/CancellationGroup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using UniCore.Extensions.Language;
+
+namespace UniCore.Helpers
+{
+    public class CancellationGroup<T> : IDisposable
+    {
+        private Dictionary<T, CancellationTokenSource> _tokens;
+
+        public void Dispose()
+        {
+            if (_tokens.IsNullOrEmpty())
+            {
+                return;
+            }
+
+            foreach (KeyValuePair<T, CancellationTokenSource> pair in _tokens)
+            {
+                pair.Value.CancelAndDispose();
+            }
+        }
+
+        public CancellationTokenSource Renew(T key)
+        {
+            _tokens ??= new();
+
+            if (_tokens.ContainsKey(key))
+            {
+                _tokens[key].CancelAndDispose();
+            }
+
+            CancellationTokenSource token = new();
+            _tokens[key] = token;
+
+            return token;
+        }
+
+        public bool Cancel(T key)
+        {
+            if (_tokens.IsNullOrEmpty())
+            {
+                return false;
+            }
+
+            if (!_tokens.ContainsKey(key))
+            {
+                return false;
+            }
+
+            _tokens[key].CancelAndDispose();
+
+            return _tokens.Remove(key);
+        }
+
+        public bool Contains(T key)
+        {
+            if (_tokens.IsNullOrEmpty())
+            {
+                return false;
+            }
+
+            return _tokens.ContainsKey(key);
+        }
+    }
+}
