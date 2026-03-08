77e3f9c5f071c9e80191056992f6948bc621b60e
Nevrozelya <martinchertier@gmail.com>
2026-03-08 14:37:26 +0100

    feat: from/to dictionnary extensions for ArrayGrid

---
+++ Assets/Runtime/Helpers/Grid/ArrayGridExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UniCore.Extensions.Language;
+
+namespace UniCore.Helpers.Grid
+{
+    public static class ArrayGridExtensions
+    {
+        public static Dictionary<Coordinates, T> ToDictionary<T>(this ArrayGrid<T> grid)
+        {
+            if (grid == null)
+            {
+                return null;
+            }
+
+            Dictionary<Coordinates, T> dictionary = null;
+
+            grid.ForWithValue((c, t) =>
+            {
+                dictionary ??= new();
+                dictionary[c] = t;
+            });
+
+            return dictionary;
+        }
+
+        public static ArrayGrid<T> FromDictionary<T>(this Dictionary<Coordinates, T> dictionary)
+        {
+            if (dictionary.IsNullOrEmpty())
+            {
+                return null;
+            }
+
+            Dictionary<Coordinates, T>.KeyCollection keys = dictionary.Keys;
+            int width = keys.Max(k => k.X) + 1;
+            int height = keys.Max(k => k.Y) + 1;
+
+            ArrayGrid<T> grid = new(width, height);
+
+            foreach (KeyValuePair<Coordinates, T> pair in dictionary)
+            {
+                grid[pair.Key] = pair.Value;
+            }
+
+            return grid;
+        }
+    }
+}
